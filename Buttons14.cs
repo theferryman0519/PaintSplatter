95592734d13f09c6c1140eb8906ca233650a6b97
The Ferryman <82970765+theferryman0519@users.noreply.github.com>
2021-09-28 15:33:46 -0500

    Add files via upload

---
+++ Buttons14.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using System.Data;
+using System.IO;
+using System.Net;
+
+public class Buttons14 : MonoBehaviour {
+		
+// ---------------------------------------- START: LIST OF VARIABLES ----------------------------------------
+// --------------- PUBLIC VARIABLES ---------------
+	public Button BackButton;
+	
+// --------------- PRIVATE VARIABLES ---------------
+	
+	
+// --------------- STATIC VARIABLES ---------------
+	
+	
+// --------------- FIREBASE VARIABLES ---------------
+	
+	
+// ---------------------------------------- END: LIST OF VARIABLES ----------------------------------------
+// ---------------------------------------- START: CALLING OTHER SCRIPTS ----------------------------------------
+	// Scene Change
+	public SceneChange Scene03LoadRun;
+	
+// ---------------------------------------- END: CALLING OTHER SCRIPTS ----------------------------------------
+// ---------------------------------------- START: INITIAL FUNCTIONS ----------------------------------------
+// --------------- START FUNCTION ---------------
+	void Start() {
+		// BackButton
+		Button BackButtonClick = BackButton.GetComponent<Button>();
+		BackButtonClick.onClick.AddListener(BackButtonClicking);
+	}
+	
+// --------------- AWAKE FUNCTION ---------------
+	void Awake() {
+		
+	}
+	
+// --------------- UPDATE FUNCTION ---------------
+	void Update() {
+		
+	}
+	
+// ---------------------------------------- END: INITIAL FUNCTIONS ----------------------------------------
+// ---------------------------------------- START: OTHER FUNCTIONS ----------------------------------------
+    public void BackButtonClicking() {
+		Scene03LoadRun.Scene03Load();
+	}
+	
+// ---------------------------------------- END: OTHER FUNCTIONS ----------------------------------------
+}
---
+++ EnableObjects14.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using System.Data;
+using System.IO;
+using System.Net;
+
+public class EnableObjects14 : MonoBehaviour {
+		
+// ---------------------------------------- START: LIST OF VARIABLES ----------------------------------------
+// --------------- PUBLIC VARIABLES ---------------
+	public Image BackgroundImage;
+	public Text TitleText;
+	public Text ActionText;
+	public Image BackButton;
+	
+// --------------- PRIVATE VARIABLES ---------------
+	
+	
+// --------------- STATIC VARIABLES ---------------
+	
+	
+// --------------- FIREBASE VARIABLES ---------------
+	
+	
+// ---------------------------------------- END: LIST OF VARIABLES ----------------------------------------
+// ---------------------------------------- START: CALLING OTHER SCRIPTS ----------------------------------------
+	
+	
+// ---------------------------------------- END: CALLING OTHER SCRIPTS ----------------------------------------
+// ---------------------------------------- START: INITIAL FUNCTIONS ----------------------------------------
+// --------------- START FUNCTION ---------------
+	void Start() {
+		EnableAllObjects();
+	}
+	
+// --------------- AWAKE FUNCTION ---------------
+	void Awake() {
+		
+	}
+	
+// --------------- UPDATE FUNCTION ---------------
+	void Update() {
+		
+	}
+	
+// ---------------------------------------- END: INITIAL FUNCTIONS ----------------------------------------
+// ---------------------------------------- START: OTHER FUNCTIONS ----------------------------------------
+// --------------- ENABLING OBJECT FUNCTIONS ---------------
+	public void EnableAllObjects() {
+		BackgroundImage.enabled = true;
+		TitleText.enabled = true;
+		ActionText.enabled = true;
+		BackButton.enabled = true;
+	}
+
+// --------------- DISABLING OBJECT FUNCTIONS ---------------
+	public void DisableAllObjects() {
+		BackgroundImage.enabled = false;
+		TitleText.enabled = false;
+		ActionText.enabled = false;
+		BackButton.enabled = false;
+	}
+	
+// ---------------------------------------- END: OTHER FUNCTIONS ----------------------------------------
+}
